2f0a6ebf834db8bce3412dced532925a8c48de9b
sixologyyy <sixologyyy@icloud.com>
2018-03-24 22:18:17 +0800

    fixed some bugs of function delete when deleting tree root

--- Chapter6/BinSearchTree.cpp
+++ Chapter6/BinSearchTree.cpp
@@ -6,9 +6,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#define ARRAYLEN 10
+#define ARRAYLEN 11
 
-int source[]={55,94,6,65,11,38,91,29,67,82};
+int source[]={55,94,6,65,11,38,91,29,67,82,100};
+//int source[]={28};
 
 typedef struct BinSearchNode
 {
@@ -18,7 +19,7 @@ typedef struct BinSearchNode
 }BST;
 
 //  1.插入节点
-void InsertNodeOfBST(BST *t,int key)   //在二叉排序树中插入查找关键字key
+void InsertNodeOfBST(BST *&t,int key)   //在二叉排序树中插入查找关键字key
 {
 	BST *p,*parent,*head;
 
@@ -75,7 +76,7 @@ BST *SearchBST(BST *t,int key)
 }
 
 //  3.创建
-void CreateBST(BST *t,int data[],int n)//n个数据在数组d中，tree为二叉排序树根
+void CreateBST(BST *&t,int data[],int n)//n个数据在数组d中，tree为二叉排序树根
 {
 	int i;
 	t->data=data[0];
@@ -87,35 +88,37 @@ void CreateBST(BST *t,int data[],int n)//n个数据在数组d中，tree为二叉
 }
 
 //  4.中序遍历
-void LDRTraverse(BST *t)
+void LDRTraverse(BST *&t)
 {
-	 if(t)//树不为空，则执行如下操作 
-	 {
-		 LDRTraverse(t->left); //中序遍历左子树
-		 printf("%d ",t->data); //输出结点数据 
-		 LDRTraverse(t->right); //中序遍历右子树/
-	 }
-	 return; 
- } 
+	if(t)//树不为空，则执行如下操作 
+	{
+		LDRTraverse(t->left); //中序遍历左子树
+		printf("%d ",t->data); //输出结点数据 
+		LDRTraverse(t->right); //中序遍历右子树/
+	}
+	return; 
+} 
 
 //  5.删除节点
 /*
-删除一个节点p，并且保留二叉查找树的性质。分三种情况：
+删除一个节点p，并且保留二叉查找树的性质。分四种情况：
 （1）叶子节点，直接删除
-（2）只有左子节点或者右子节点，连接后面的子树再删除
-（3）左右子节点都存在，那么需要找到p的中序遍历前驱节点s，即p的左子树的最右下（前驱还有可能不在p的子树上，但这种情况与p有左子树矛盾）
+（2）非根，且只有左子节点或者右子节点，连接后面的子树再删除
+（3）非根，且左右子节点都存在，那么需要找到p的中序遍历前驱节点pre，即p的左子树的最右下（前驱还有可能不在p的子树上，但这种情况与p有左子树矛盾）
 
-	有两种删除方法，连接法和代替法
-	1）连接法：父亲f断掉的地方连p左，前驱s连p右
-	   f->left（或right） = p->left   s->right = p->right   free(p)
-	2）替代法：s的值代替p的值，s的父亲q右边连s左，最后删除s，特殊考虑q==p的情况
-	   因为s在最右下，所以s没有右孩子
-	   q->right = s->left  p->data = s->data     free(s)
+	有两种删除方法，连接法和替代法
+	1）连接法：父亲f断掉的地方连p左，前驱pre连p右。【无法处理p是根的情况】
+	   f->left（或right） = p->left   pre->right = p->right   free(p)
+
+	2）替代法：pre的值代替p的值，pre的父亲q右边连pre左，最后删除pre。【特殊考虑pre是p的孩子的情况】
+	   因为pre在最右下，所以pre没有右孩子，且pre是父亲的右孩子（除非pre是p的左孩子）
+	   q->right = pre->left          p->data = s->data     free(pre)
+（4）根节点，类似前面分类讨论有没有左右子树的情况
 */
- void DeleteNodeOfBST(BST *t,int key)
- {
-	BST *p,*parent;   //记录当前节点和其父亲
-	BST *l,*l1;
+void DeleteNodeOfBST(BST *&t,int key)
+{
+	BST *p=t;
+	BST *parent;       //记录当前节点和其父亲
 	int child=0;       //记录p是父亲的左还是右，0表示左子树，1表示右子树 
 	if(!t) return;     //二叉排序树为空，则退出
 
@@ -125,57 +128,126 @@ void LDRTraverse(BST *t)
 	{        
 		if(p->data==key)
 		{
-			//叶节点
+			//1.叶节点
 			if(p->left==NULL && p->right==NULL) 
 			{
 				if(p==t) //被删除的是根结点 
 				{
-					free(p);
+					//free(p);
+					p=NULL;
 					t=NULL;
+					return;
 				}
 				else if(child==0) //父结点为左子树 
 				{
 					parent->left=NULL; 
-					free(p); 
+					free(p);
+					p=NULL;
+					return;
 				}
 				else //父结点为右子树 
 				{
 					parent->right=NULL;
-					free(p); 
+					free(p);
+					p=NULL;
+					return;
 				}
 			}
-			//左子树为空，右子树不为空
+			//2.1 左子树为空，右子树不为空
 			else if(p->left==NULL)
 			{
-				if(child==0) //是父结点的左子树
-					parent->left=p->right;
-				else         //是父结点的右子树             
-					parent->right=p->right;
-				free(p);
-			}
-			//右子树为空，左子树不为空
-			else if(!p->right)
-			{
-				 if(child==0) //是父结点的左子树
-					parent->left=p->left;
-				else          //是父结点的右子树             
-					parent->right=p->left;
-				free(p);    
-			}
-			else  //左右子树都不为空 
-			{
-				l1=p; //保存左子树的父结点 
-				l=p->right; //从当前结点的右子树进行查找 
-				while(l->left) //左子树不为空 
+				if(p==t)   //根节点
 				{
-					l1=l;
-					l=l->left; //查找左子树 
+					t=p->right;
+					//free(p);
+					p=NULL;
+					return;
 				}
-				p->data=l->data; //将左子树的数据保存到被删除结点
-				l1->left=NULL; //设置父结点的左子树指针为空 
-				free(l1); //释放左子树占的内存空间
+				else
+				{
+					if(child==0) //是父结点的左子树
+						parent->left=p->right;
+					else         //是父结点的右子树             
+						parent->right=p->right;
+					free(p);
+					p=NULL;
+					return;
+				}
 			}
-			p=NULL;
+			//2.2 右子树为空，左子树不为空
+			else if(p->right==NULL)
+			{
+				if(p==t)   //根节点
+				{
+					t=p->left;
+					//free(p);
+					p=NULL;
+					return;
+				}
+				else
+				{
+					if(child==0) //是父结点的左子树
+						parent->left=p->left;
+					else          //是父结点的右子树             
+						parent->right=p->left;
+					free(p);
+					p=NULL;
+					return;
+				}
+			}
+			//3.左右子树都不为空 
+			else
+			{
+				//3.1 p不为根，用连接法
+				if(p!=t)
+				{
+					//寻找p左子树最右下
+					BST *pre=p->left;
+					while(pre->right)
+					{
+						pre=pre->right;
+					}
+					//连接父亲的断开处
+					if(child==0)
+						parent->left=p->left;
+					else
+						parent->right=p->left;
+					//连接前驱和p右
+					pre->right=p->right;
+					free(p);
+					p=NULL;
+					return;
+				}
+				//3.2 p为根，用替代法，要考虑pre是否为p的孩子
+				else
+				{
+					BST *pre=p->left;
+					if(pre->right==NULL)  //p左子树没有右孩子，说明p的前驱就是p左
+					{
+						//修改p的关键字
+						p->data=pre->data;
+						//连接
+						p->left=pre->left;
+						free(pre);
+						pre=NULL;
+						return;
+					}
+					else
+					{
+						BST *q=pre;   //记录pre的父亲
+						while(pre->right)
+						{
+							q=pre;
+							pre=pre->right;
+						}
+						p->data=pre->data;
+						q->right=pre->left;
+						free(pre);
+						pre=NULL;
+						return;
+					}
+				}
+			}
 		}
 		//左子树继续遍历
 		else if(key<p->data) 
@@ -197,23 +269,24 @@ int main()
 {
 	int i,key;
 	BST bst,*pos;
+	BST *pbst=&bst;
 	printf("原数据:"); 
 	for(i=0;i<ARRAYLEN;i++)
 		printf("%d ",source[i]);
 	printf("\n");
 
-	CreateBST(&bst,source,ARRAYLEN);
+	CreateBST(pbst,source,ARRAYLEN);
 
 	printf("遍历二叉排序树:"); 
-	LDRTraverse(&bst);
+	LDRTraverse(pbst);
 
 	do
 	{
 		printf("\n输入待删除关键字：\n");
 		scanf("%d",&key);
-		DeleteNodeOfBST(&bst,key);
+		DeleteNodeOfBST(pbst,key);
 		printf("删除结点值为%d后:",key); 
-		LDRTraverse(&bst);
+		LDRTraverse(pbst);
 
 	}while(key!=-1);
 
