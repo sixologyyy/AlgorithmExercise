594c59c097d671ab4fbd2952428dd88557ff2285
sixologyyy <sixologyyy@icloud.com>
2018-03-25 20:36:47 +0800

    add IndexSearch.cpp

--- Chapter6/IndexSearch.cpp
+++ Chapter6/IndexSearch.cpp
@@ -1,32 +1,37 @@
 #include <stdio.h>
+
 #define INDEXTABLE_LEN 3
 #define TABLE_LEN 30
+
 typedef struct item
 {
     int index;    //索引值 
     int start;    //开始位置 
     int length;   //子表长度 
-}suoyin;
+}INDEX;
+
 //定义主表数据 
-long zhu[TABLE_LEN]={
+long mainTable[TABLE_LEN]={
      1080101,1080102,1080103,1080104,1080105,1080106,0,0,0,0,
      1080201,1080202,1080203,1080204,0,0,0,0,0,0,
      1080301,1080302,1080303,1080304,0,0,0,0,0,0};
+
 //定义索引表 
-suoyin indextable[INDEXTABLE_LEN]={
+INDEX indexTable[INDEXTABLE_LEN]={
     {10801,0,6},
     {10802,10,4},
     {10803,20,4}};
+
 int IndexSearch(int key) //按索引查找 
 {
     int i,index1,start,length;
-    index1=key/100;//计算索引值
+    index1=key/100;       //计算索引值
     for(i=0;i<INDEXTABLE_LEN;i++) //在索引表中查找索引值 
     {
-        if(indextable[i].index==index1) //找到索引值 
+        if(indexTable[i].index==index1) //找到索引值 
         {
-            start=indextable[i].start; //获取数组开始序号 
-            length=indextable[i].length; //获取元素长度 
+            start=indexTable[i].start; //获取数组开始序号 
+            length=indexTable[i].length; //获取元素长度 
             break; //跳出循环 
         }
     }
@@ -34,37 +39,38 @@ int IndexSearch(int key) //按索引查找
         return -1;//索引表中查找失败 
     for(i=start;i<start+length;i++)
     {
-        if(zhu[i]==key) //找到关键字 
+        if(mainTable[i]==key) //找到关键字 
             return i; //返回序号 
     }
     return -1; //查找失败，返回-1 
 }
-int InsertNode(key)
+
+int InsertNode(int key)
 {
     int i,index1,start,length;
     index1=key/100;//计算索引值
     for(i=0;i<INDEXTABLE_LEN;i++) //在索引表中查找索引值 
     {
-        if(indextable[i].index==index1) //找到索引值 
+        if(indexTable[i].index==index1) //找到索引值 
         {
-            start=indextable[i].start; //获取数组开始序号 
-            length=indextable[i].length; //获取元素长度 
+            start=indexTable[i].start; //获取数组开始序号 
+            length=indexTable[i].length; //获取元素长度 
             break; //跳出循环 
         }
     }
     for(i=0;i<INDEXTABLE_LEN;i++) //在索引表中查找索引值 
     {
-        if(indextable[i].index==index1) //找到索引值 
+        if(indexTable[i].index==index1) //找到索引值 
         {
-            start=indextable[i].start; //获取数组开始序号 
-            length=indextable[i].length; //获取元素长度 
+            start=indexTable[i].start; //获取数组开始序号 
+            length=indexTable[i].length; //获取元素长度 
             break; //跳出循环 
         }
     }
     if(i>=INDEXTABLE_LEN)
-        return -1;//索引表中查找失败     
-    zhu[start+length]=key;//保存关键字到主表 
-    indextable[i].length++;//修改索引表中的子表长度
+        return -1;//索引表中查找失败
+    mainTable[start+length]=key;//保存关键字到主表 
+    indexTable[i].length++;//修改索引表中的子表长度
     return 0;    
 }
 
@@ -74,7 +80,7 @@ int main()
     int i,pos;
     printf("原数据:"); 
     for(i=0;i<TABLE_LEN;i++)
-        printf("%ld ",zhu[i]);
+        printf("%ld ",mainTable[i]);
     printf("\n");
     printf("输入查找关键字:");
     scanf("%ld",&key);
@@ -90,9 +96,8 @@ int main()
     else
     {
         for(i=0;i<TABLE_LEN;i++)
-            printf("%ld ",zhu[i]);
+            printf("%ld ",mainTable[i]);
         printf("\n");
     }    
-    getch();
     return 0;
 }
